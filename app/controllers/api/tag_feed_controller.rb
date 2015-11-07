ef19ef3bb90ca108061d2c4d47a294fe92352002
m3libea <rociorommor@gmail.com>
2015-11-06 19:54:25 -0800

    Added create endpoint for TagFeed

--- app/controllers/api/tag_feed_controller.rb
+++ app/controllers/api/tag_feed_controller.rb
@@ -9,6 +9,19 @@ class Api::TagFeedController < ApplicationController
   def show
     respond_with TagFeed.find(params[:id])
   end
+
+  def create
+    tf = TagFeed.new(tf_params)
+    if tf.save
+        render json: tf, status: 201, location:[:api, tf]
+    else
+        render json: { errors: tf.errors}, status: 422
+    end
+  end  
  
+  private
+    def tf_params
+        params.permit(:hashtag, :startDate, :endDate, :initialTagID, :latestTagID, :isComplete)
+    end  
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
-        resources :tag_feed, :only=>[:show, :index]
+        resources :tag_feed, :only=>[:show, :index, :create]
   end
 
   # You can have the root of your site routed with "root"
